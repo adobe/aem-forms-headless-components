50c14a7ad937ed4cb6535c4e89886571f48315c7
Aabha Soni <aabhas@adobe.com>
2023-06-22 15:01:27 +0530

    [FORMS-9369] implement slider and rating component

--- packages/material-ui-based-components/src/utils/mappings.ts
+++ packages/material-ui-based-components/src/utils/mappings.ts
@@ -1,9 +1,13 @@
 import Form from '../components/Form';
 import Panel from '../components/Panel';
 import TextField from '../components/TextField';
+import Rating from '../components/Rating';
+import Slider from '../components/Slider';
 
 export default {
   form: Form,
   panel: Panel,
-  'text-input': TextField
+  'text-input': TextField,
+  'custom:rating':Rating,
+  'custom:slider':Slider
 };
