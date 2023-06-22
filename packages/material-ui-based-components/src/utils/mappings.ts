509a60e7ca57a25cd129e6561ff445a72883e5bd
Aabha Soni <aabhas@adobe.com>
2023-06-22 13:07:32 +0530

    [FORMS-9320] accordion component implementation

--- packages/material-ui-based-components/src/utils/mappings.ts
+++ packages/material-ui-based-components/src/utils/mappings.ts
@@ -1,9 +1,11 @@
 import Form from '../components/Form';
 import Panel from '../components/Panel';
 import TextField from '../components/TextField';
+import Accordion from '../components/Accordion';
 
 export default {
   form: Form,
   panel: Panel,
-  'text-input': TextField
+  'text-input': TextField,
+  'custom:accordion': Accordion
 };
