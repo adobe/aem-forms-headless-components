8644d4c1ad32b91f4b7d9c926f2034ddef7f636e
Aabha Soni <aabhas@adobe.com>
2023-06-22 15:09:28 +0530

    [FORMS-9411] implement plain text component

--- packages/material-ui-based-components/src/utils/mappings.ts
+++ packages/material-ui-based-components/src/utils/mappings.ts
@@ -1,9 +1,11 @@
 import Form from '../components/Form';
 import Panel from '../components/Panel';
 import TextField from '../components/TextField';
+import PlainText from '../components/PlainText';
 
 export default {
   form: Form,
   panel: Panel,
-  'text-input': TextField
+  'text-input': TextField,
+  'plain-text': PlainText
 };
