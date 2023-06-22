95116735126b8d3ac86ec6c08f6dfd8dd7f6e46f
Aabha Soni <aabhas@adobe.com>
2023-06-22 14:34:19 +0530

    [FORMS-9143] implement radio button component

--- packages/material-ui-based-components/src/utils/mappings.ts
+++ packages/material-ui-based-components/src/utils/mappings.ts
@@ -1,9 +1,11 @@
 import Form from '../components/Form';
 import Panel from '../components/Panel';
 import TextField from '../components/TextField';
+import Radiobutton from '../components/Radiobutton';
 
 export default {
   form: Form,
   panel: Panel,
-  'text-input': TextField
+  'text-input': TextField,
+  'radio-group': Radiobutton
 };
