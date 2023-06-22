d13b90fe735a54c28ec3812c76c0e80ad9e9645e
Aabha Soni <aabhas@adobe.com>
2023-06-22 14:57:43 +0530

    [FORMS-9322] implement wizard component

--- packages/material-ui-based-components/src/utils/mappings.ts
+++ packages/material-ui-based-components/src/utils/mappings.ts
@@ -1,9 +1,11 @@
 import Form from '../components/Form';
 import Panel from '../components/Panel';
 import TextField from '../components/TextField';
+import Wizard from '../components/Wizard';
 
 export default {
   form: Form,
   panel: Panel,
-  'text-input': TextField
+  'text-input': TextField,
+  'custom:wizard': Wizard
 };
