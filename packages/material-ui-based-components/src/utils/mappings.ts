4332f268ff7eb332e25cd748ea61470080410108
Aabha Soni <aabhas@adobe.com>
2023-06-22 14:54:07 +0530

    [FORMS-9321] implement vertical and horizontal tabs component

--- packages/material-ui-based-components/src/utils/mappings.ts
+++ packages/material-ui-based-components/src/utils/mappings.ts
@@ -1,9 +1,13 @@
 import Form from '../components/Form';
 import Panel from '../components/Panel';
 import TextField from '../components/TextField';
+import TabsVerticalComp from '../components/TabsVerticalComp';
+import TabsHorizontalComp from '../components/TabsHorizontalComp';
 
 export default {
   form: Form,
   panel: Panel,
-  'text-input': TextField
+  'text-input': TextField,
+  'custom:vertical-tab': TabsVerticalComp,
+  'custom:horizontal-tab': TabsHorizontalComp
 };
