8d56df4a3bb5ff9b7bcd4c6da0c9ca3d04210f01
Aabha Soni <aabhas@adobe.com>
2023-06-22 14:48:26 +0530

    [FORMS-9319] implement button component

--- packages/material-ui-based-components/src/utils/mappings.ts
+++ packages/material-ui-based-components/src/utils/mappings.ts
@@ -1,9 +1,11 @@
 import Form from '../components/Form';
 import Panel from '../components/Panel';
 import TextField from '../components/TextField';
+import Button from '../components/Button';
 
 export default {
   form: Form,
   panel: Panel,
-  'text-input': TextField
+  'text-input': TextField,
+  button: Button
 };
