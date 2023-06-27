6f9193c74a55e6caf6dc4578b0657983a864cbfd
Aabha Soni <aabhas@adobe.com>
2023-06-27 17:05:28 +0530

    [FORMS-9411] implementing title component

--- packages/material-ui-based-components/src/utils/mappings.ts
+++ packages/material-ui-based-components/src/utils/mappings.ts
@@ -2,10 +2,12 @@ import Form from '../components/Form';
 import Panel from '../components/Panel';
 import TextField from '../components/TextField';
 import PlainText from '../components/PlainText';
+import Title from '../components/Title';
 
 export default {
   form: Form,
   panel: Panel,
   'text-input': TextField,
-  'plain-text': PlainText
+  'plain-text': PlainText,
+  title: Title
 };
