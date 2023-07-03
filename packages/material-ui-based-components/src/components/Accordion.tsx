78dd83d2efae208c9c8cc983475258a4c12fabdf
Aabha Soni <aabhas@adobe.com>
2023-07-03 18:44:10 +0530

    implementing disabled and visible props, others

--- packages/material-ui-based-components/src/components/Accordion.tsx
+++ packages/material-ui-based-components/src/components/Accordion.tsx
@@ -1,8 +1,7 @@
-import React, { useContext } from 'react';
+import React, { useCallback, useContext } from 'react';
 import { getRenderer, FormContext } from '@aemforms/af-react-renderer';
 import { withRuleEnginePanel } from '../shared/withRuleEngine';
 import { PROPS_PANEL } from '../utils/types';
-
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
@@ -13,24 +12,23 @@ const AccordionComponent = function (props: PROPS_PANEL) {
     const [expanded, setExpanded] = React.useState<number>(0);
     // @ts-ignore
     const mappings = useContext(FormContext).mappings;
-    const items = props.items || [];
+    const items = props.visible ? props.items : [];
 
-    const handleChange = (index: number) => {
+    const handleChange = useCallback((index: number) => {
         setExpanded(index);
-    };
+    }, []);
 
-    const getChild = (child: any, index: number) => {
+    const getChild = useCallback((child: any, index: number) => {
         const Comp = getRenderer(child, mappings);
         return Comp ? <Comp key={`${child.id}_${index}`} {...child} /> : (null);
-    };
-
+    }, []);
 
     return (
         <div>
-            <div>{props?.label?.value ? <div>{props.label.value}</div> : null}</div>
+            {props?.label?.visible ? <div>{props.label.value}</div> : null}
             {
                 items.map((child: any, index: number) => (
-                    <Accordion expanded={expanded === index} onChange={() => { handleChange(index); }} key={`${child.id}_${index}`}>
+                    <Accordion expanded={expanded === index} onChange={() => { handleChange(index); }} key={`${child.id}`} disabled={!props.enabled}>
                         <AccordionSummary
                             expandIcon={<ExpandMoreIcon />}
                             id={child.id}
