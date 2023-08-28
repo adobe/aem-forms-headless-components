178b90b51334b7b6b50d1a91f7e048e7af52f701
Amit Kumar <amitkumar@adobe.com>
2023-08-28 12:10:48 +0530

    add ADOBE CONFIDENTIAL header

--- packages/material-ui-based-components/src/components/Form.tsx
+++ packages/material-ui-based-components/src/components/Form.tsx
@@ -1,3 +1,23 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2023 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+
+* Adobe permits you to use and modify this file solely in accordance with
+* the terms of the Adobe license agreement accompanying it.
+*************************************************************************/
+
 import React, { useContext } from 'react';
 import { renderChildren, FormContext } from '@aemforms/af-react-renderer';
 import { withRuleEnginePanel } from '../shared/withRuleEngine';
@@ -8,10 +28,10 @@ const Form = function (props: PROPS_PANEL) {
   // @ts-ignore
   const context = useContext(FormContext);
   return (
-    <div>
+    <form>
       {restProps?.label?.visible ? <div>{restProps.label.value}</div> : null}
       {renderChildren(restProps, context.mappings, context.modelId, handlers)}
-    </div>
+    </form>
   );
 };
 
--- packages/material-ui-based-components/src/utils/mappings.ts
+++ packages/material-ui-based-components/src/utils/mappings.ts
@@ -1,3 +1,23 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2023 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+
+* Adobe permits you to use and modify this file solely in accordance with
+* the terms of the Adobe license agreement accompanying it.
+*************************************************************************/
+
 import Form from '../components/Form';
 import Panel from '../components/Panel';
 import TextField from '../components/TextField';
@@ -52,6 +72,7 @@ export default {
   'text-input': TextField,
   'custom:accordion': Accordion
   'text-input': TextField,
+  'custom:switch': Switch,
   'password-input': TextField,
   'multiline-input': TextField,
   email:TextField
